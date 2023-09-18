2aab24710a3e27ba22214d72f1100d79096c7152
HanawuZ <b6302245@gmail.com>
2023-09-18 09:54:07 +0700

    init java spring be

---
+++ springbe/src/main/java/com/example/springbe/greeting/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.springbe.greeting;
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+	public Map<String, Object> greeting() {
+		return Collections.singletonMap("message", "Hello, World");
+	}
+}
